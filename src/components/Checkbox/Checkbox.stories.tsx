7ad306da5d86f7deb34607503744771582e64671
insurpartsKYS <ys.kim@insurparts.com>
2022-11-24 14:56:41 +0900

    [feature] Checkbox 컴포넌트 구현

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { FormControl, FormControlLabel } from '@mui/material';
+import { IuiThemeProvider } from '../../system';
+import { COMPONENTS_TITLE, INPUTS_TITLE } from '../../constants';
+import Checkbox from './Checkbox';
+
+export default {
+  title: `${COMPONENTS_TITLE}/${INPUTS_TITLE}/Checkbox`,
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+const StandAloneTemplate: ComponentStory<typeof Checkbox> = (args) => {
+  return (
+    <IuiThemeProvider mode="admin">
+      <Checkbox {...args} />
+      <Checkbox {...args} defaultChecked />
+      <Checkbox {...args} disabled checked />
+    </IuiThemeProvider>
+  );
+};
+
+const WithFormControlTemplate: ComponentStory<typeof Checkbox> = (args) => {
+  return (
+    <IuiThemeProvider mode="admin">
+      <FormControl>
+        <FormControlLabel
+          control={<Checkbox {...args} />}
+          label="공업사에서 타업체 연결 요청"
+        />
+        <FormControlLabel
+          control={<Checkbox {...args} defaultChecked />}
+          label="공업사에서 타업체 연결 요청"
+        />
+        <FormControlLabel
+          control={<Checkbox {...args} disabled checked />}
+          label="공업사에서 타업체 연결 요청"
+        />
+      </FormControl>
+    </IuiThemeProvider>
+  );
+};
+
+export const StandAlone = StandAloneTemplate.bind({});
+export const WithFormControl = WithFormControlTemplate.bind({});
+
+StandAlone.args = {};
+
+WithFormControl.args = {};
---
+++ src/icons/CheckboxUnChecked.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createSvgIcon } from '@mui/material';
+
+export default createSvgIcon(
+  <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="19" height="19" stroke="currentColor" />
+  </svg>,
+  'CheckboxUnChecked'
+);
