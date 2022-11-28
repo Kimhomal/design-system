36a6fcf3431b9618117ec1efbb97fab4c545e313
insurpartsKYS <ys.kim@insurparts.com>
2022-11-28 10:18:10 +0900

    [feature] Switch 컴포넌트 구현

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Switch as MuiSwitch,
+  switchClasses,
+  SwitchProps,
+  touchRippleClasses,
+} from '@mui/material';
+import {
+  blueGrey,
+  common,
+  grey,
+  insurBlue,
+  lineGrey,
+} from '../../system/colors';
+import { deepmerge } from '../../utils';
+
+const Switch = (props: SwitchProps) => {
+  const { sx, ...rest } = props;
+
+  return (
+    <MuiSwitch
+      sx={deepmerge(
+        {
+          width: '44px',
+          height: '24px',
+          margin: '8px',
+          padding: 0,
+          [`.${switchClasses.switchBase}`]: {
+            padding: '2px',
+            [`&.${switchClasses.checked}`]: {
+              color: common.white,
+            },
+            [`&.${switchClasses.checked}+.${switchClasses.track}`]: {
+              opacity: 1,
+            },
+          },
+          [`.${switchClasses.thumb}`]: {
+            boxShadow: 'none',
+          },
+          [`.${switchClasses.track}`]: {
+            borderRadius: '20px',
+            opacity: 1,
+            backgroundColor: blueGrey[300],
+          },
+        },
+        sx
+      )}
+      {...rest}
+    />
+  );
+};
+
+export default Switch;
