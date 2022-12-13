5ff6cd86fef0766c02d6d443d912da9e7ad759f9
insurpartsKYS <ys.kim@insurparts.com>
2022-12-13 16:35:56 +0900

    [feature] Select 컴포넌트 구현

---
+++ src/components/MenuItem/MenuItem.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import MenuItem from './MenuItem';
+
+describe('MenuItem', () => {
+  test('renders the MenuItem component', () => {
+    render(<MenuItem />);
+  });
+});
