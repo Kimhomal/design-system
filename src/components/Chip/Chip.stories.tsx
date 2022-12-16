d9df3658bf56174deff7641caad207b440745fb4
insurpartsKYS <ys.kim@insurparts.com>
2022-12-16 15:58:05 +0900

    [feature] Chip 컴포넌트에 색상타입 추가

--- src/components/Chip/Chip.stories.tsx
+++ src/components/Chip/Chip.stories.tsx
@@ -17,19 +17,105 @@ const ChipTehmplate: ComponentStory<typeof Chip> = (args) => {
 
   return (
     <IuiThemeProvider mode="admin">
-      <Stack direction="row" spacing={1}>
-        <Chip
-          label="배송예정일"
-          color="primary"
-          onClick={handleClick}
-          {...args}
-        />
-        <Chip
-          label="사고차량 입고일"
-          variant="outlined"
-          onClick={handleClick}
-          {...args}
-        />
+      <Stack spacing={2}>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            label="배송예정일"
+            color="primary"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="사고차량 입고일"
+            variant="outlined"
+            onClick={handleClick}
+            {...args}
+          />
+        </Stack>
+
+        <Stack direction="row" spacing={1}>
+          <Chip
+            label="연결중"
+            clickable={false}
+            type="request"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="주문확정"
+            clickable={false}
+            type="confirmOrder"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="청구대기"
+            clickable={false}
+            type="billWait"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="출고완료"
+            clickable={false}
+            type="vehicleRelease"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="청구완료"
+            clickable={false}
+            type="billComplete"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="취소"
+            clickable={false}
+            type="canceled"
+            onClick={handleClick}
+            {...args}
+          />
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            label="활성화"
+            clickable={false}
+            type="active"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="비활성화"
+            clickable={false}
+            type="deactive"
+            onClick={handleClick}
+            {...args}
+          />
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            label="부품사"
+            clickable={false}
+            type="parts"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="공업사"
+            clickable={false}
+            type="repairshop"
+            onClick={handleClick}
+            {...args}
+          />
+          <Chip
+            label="보험사"
+            clickable={false}
+            type="insurance"
+            onClick={handleClick}
+            {...args}
+          />
+        </Stack>
       </Stack>
     </IuiThemeProvider>
   );
--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -3,8 +3,87 @@ import { Chip as MuiChip, chipClasses, ChipProps } from '@mui/material';
 import { blueGrey, common, insurBlue } from '../../system/colors';
 import { deepmerge } from '../../utils';
 
-const Chip = (props: ChipProps) => {
-  const { sx, ...rest } = props;
+type RequestStatus =
+  | 'request'
+  | 'confirmOrder'
+  | 'billWait'
+  | 'vehicleRelease'
+  | 'billComplete'
+  | 'canceled';
+
+type AgentStatus = 'active' | 'deactive';
+
+type AgentType = 'parts' | 'repairshop' | 'insurance';
+
+type ChipType = {
+  type?: RequestStatus & AgentStatus & AgentType;
+};
+
+const Chip = (props: ChipProps & ChipType) => {
+  const { sx, type, ...rest } = props;
+
+  function backgroundColor(type: ChipType['type']) {
+    if (!type) {
+      return;
+    }
+
+    switch (type) {
+      // 요청 상태
+      case 'request':
+        return '#3F54D1';
+      case 'confirmOrder':
+        return '#2AD4AF';
+      case 'billWait':
+        return '#15CDCB';
+      case 'vehicleRelease':
+        return '#60BFF7';
+      case 'billComplete':
+        return '#5280E2';
+      case 'canceled':
+        return '#FF0400';
+      // 업체 상태
+      case 'active':
+        return '#DCEBFF';
+      case 'deactive':
+        return '#D1D6DB';
+      // 업체 타입
+      case 'parts':
+        return '#EDF3FF';
+      case 'repairshop':
+        return '#FFEFEF';
+      case 'insurance':
+        return '#EAFEF0';
+    }
+  }
+
+  function textColor(type: ChipType['type']) {
+    if (!type) {
+      return;
+    }
+
+    switch (type) {
+      // 요청 상태
+      case 'request':
+      case 'confirmOrder':
+      case 'billWait':
+      case 'vehicleRelease':
+      case 'billComplete':
+      case 'canceled':
+        return '#FFF';
+      // 업체 상태
+      case 'active':
+        return '#0046A5';
+      case 'deactive':
+        return '#6B7684';
+      // 업체 타입
+      case 'parts':
+        return '#3B69E6';
+      case 'repairshop':
+        return '#FF0400';
+      case 'insurance':
+        return '#00864B';
+    }
+  }
 
   return (
     <MuiChip
@@ -28,6 +107,9 @@ const Chip = (props: ChipProps) => {
               backgroundColor: common.white,
             },
           },
+
+          backgroundColor: backgroundColor(type),
+          color: textColor(type),
         },
         sx
       )}
