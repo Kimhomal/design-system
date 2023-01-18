3251fc9e45b8ebde93c9293c24df4d3468353297
insurpartsKYS <ys.kim@insurparts.com>
2023-01-18 16:22:23 +0900

    [feature] TextField 컴포넌트 구현

--- src/icons/DropDown.tsx
+++ src/icons/DropDown.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@mui/material';
+import { deepmerge } from '../utils';
 
 export default function DropDown(props: SvgIconProps) {
+  const { sx, ...rest } = props;
+
   return (
     <SvgIcon
       viewBox="0 0 12 6"
-      sx={{
-        width: '12px',
-      }}
-      {...props}
+      sx={deepmerge(
+        {
+          width: '12px',
+        },
+        sx
+      )}
+      {...rest}
     >
       <path
         d="M6 6L0.803847 -2.51245e-08L11.1962 8.834e-07L6 6Z"
