9f6c03acddf01bd919f014a7fd00a8af12c03541
insurpartsKYS <ys.kim@insurparts.com>
2023-02-20 15:02:19 +0900

    [fix] Radio와 Checkbox 아이콘 배경색상 흰색 적용

--- src/icons/CheckboxUnChecked.tsx
+++ src/icons/CheckboxUnChecked.tsx
@@ -3,7 +3,14 @@ import { createSvgIcon } from '@mui/material';
 
 export default createSvgIcon(
   <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="0.5" y="0.5" width="19" height="19" stroke="currentColor" />
+    <rect
+      x="0.5"
+      y="0.5"
+      width="19"
+      height="19"
+      fill="#FFF"
+      stroke="currentColor"
+    />
   </svg>,
   'CheckboxUnChecked'
 );
--- src/icons/RadioUnChecked.tsx
+++ src/icons/RadioUnChecked.tsx
@@ -4,11 +4,10 @@ import { createSvgIcon } from '@mui/material';
 export default createSvgIcon(
   <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19Z"
+      d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10Z"
       fill="currentcolor"
     />
+    <circle cx="10" cy="10" r="9" fill="#FFF" />
   </svg>,
   'RadioUnChecked'
 );
